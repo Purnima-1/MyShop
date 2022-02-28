d2c53f974b243017cabe1b164ab7d338ab8c543d
Purnima-1 <baranwal.purnimagmail.com>
2022-02-27 16:16:35 -0800

    added header and footer component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
+import { Container } from "react-bootstrap";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 
 const App = () => {
   return (
     <>
+    <Header/>
+      <Container>
+    <main>
     <h1>Welcome to MyShop</h1>
+    </main>
+    </Container>
+    <Footer/>
     </>
   );
 }
