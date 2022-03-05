8fc48e8c47169e41868667eca0b553a676469128
Purnima-1 <baranwal.purnimagmail.com>
2022-03-05 13:15:49 -0800

    created components-Product,Rating,screens

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,21 @@
 import { Container } from "react-bootstrap";
 import Footer from "./components/Footer";
+
+
 import Header from "./components/Header";
+import HomeScreen from "./screens/HomeScreen";
 
 
 const App = () => {
   return (
     <>
     <Header/>
-      <Container>
     <main>
-    <h1>Welcome to MyShop</h1>
-    </main>
+      <Container>
+    <HomeScreen/>
     </Container>
-    <Footer/>
+    </main>
+    <Footer/>    
     </>
   );
 }
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import { Container, Nav, Navbar } from 'react-bootstrap'
 
 const Header = () => {
   return (
-    <header>Header</header>
+    <header>
+      <Navbar bg='light' expand='lg' collapseOnSelect>
+      <Container>
+        <Navbar.Brand href = "/">MyShop</Navbar.Brand>
+        <Navbar.Toggle aria-controls='basic-navbar-nav' />
+        <Navbar.Collapse id = 'basic-navbar-nav'>
+          <Nav className='ms-auto'>
+            <Nav.Link href = '/cart'><i className='fas fa-shopping-cart mx-1'></i>Cart</Nav.Link>
+            <Nav.Link href = '/cart'><i className='fas fa-user mx-1'></i>Sign In</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+      </Container>
+
+      </Navbar>
+    </header>
   )
 }
 
