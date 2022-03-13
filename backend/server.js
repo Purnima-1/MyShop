42efb27bef5689a15b2931c84cab65abf84b818c
Purnima-1 <baranwal.purnimagmail.com>
2022-03-13 16:41:35 -0700

    connected to database

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,17 @@
-const express = require('express')
-const products = require ('./data/products')
+import express from 'express';
+import dotenv from 'dotenv';
+import products from './data/products.js';
+import connectDB from './config/db.js';
+
 
 const app = express()
+dotenv.config();
+connectDB();
 
+app.get('/', (req,res) => {
+    res.json({'msg': "Hello"})
+    })
+  
 app.get('/api/products', (req,res) => {
 res.json(products)
 })
@@ -12,4 +21,6 @@ app.get('/api/products/:id', (req,res) => {
     res.json(product);
 })
 
-app.listen(5000, console.log("Server is running on port 5000"));
+const PORT = process.env.PORT || 5000
+app.listen(PORT, console.log(`Server is running on port ${PORT}`));
+
