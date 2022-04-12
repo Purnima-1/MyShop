796543a884d9ffa580985f2d062ed16c19372236
Purnima-1 <baranwal.purnimagmail.com>
2022-04-12 00:11:04 -0700

    ProfileScreen,RegisterScreen added,registerUser,updateProfileUser,getuser in backend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,6 @@
 import asyncHandler from "express-async-handler";
 import Users from "../models/userModel.js";
-import bcryptjs from "bcryptjs";
+//import bcryptjs from "bcryptjs";
 import generateToken from "../utils/generateToken.js";
 
 //@desc Auth user & get token
@@ -26,7 +26,7 @@ const authUser = asyncHandler(async (req, res) => {
 });
 
 //@desc Get user profile
-//@route POST api/users/profile
+//@route GET api/users/profile
 //@access Private
 const getUserProfile = asyncHandler(async(req,res) => {
   const user = await Users.findById(req.user._id)
@@ -72,4 +72,32 @@ const registerUser = asyncHandler(async(req,res) => {
   }
 })
 
-export {authUser,registerUser,getUserProfile}
+//@desc Update user profile
+//@route PUT api/users/profile
+//@access Private
+
+const updateUserProfile = asyncHandler(async(req,res) => {
+  const user = await Users.findById(req.user._id)
+
+  if(user) {
+    user.name = req.body.name || user.name
+    user.email = req.body.email || user.email
+    if(req.body.password){
+      user.password = req.body.password
+    }
+    const updatedUser = await user.save()
+
+    res.json({
+      id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+      token: generateToken(updatedUser._id)
+    })
+  } else{
+    res.status(404)
+    throw new Error('User not found')
+  }
+})
+
+export {authUser,registerUser,getUserProfile,updateUserProfile}
